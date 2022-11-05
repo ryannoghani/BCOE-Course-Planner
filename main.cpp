0eddf4b6caa0d6ae4c4738e4e64e0d237d9e488c
Ryan Noghani <ryannoghani@ucr-secure-38-10-13-201-153.wnet.ucr.edu>
2022-11-05 14:50:20 -0700

    added menu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
-
+#include <string>
+#include <vector>
+#include <fstream>
+#include "course_names.txt"
 using namespace std;
 
+
+
+bool is_a_course(const string&);
+
+
 int main() {
-    cout << "Hello World" << endl;
+    string course = "";
+    vector<string> takenCourses;
+    cout << "Enter any courses you have credit for. Type \"done\" when all the courses are entered\n" << endl;
+
+    cin >> course;
+
+    while (course != "done") {
+        if (is_a_course(course)) {
+            takenCourses.push_back(course);
+        }
+
+        cin >> course;
+    }
+
+
     return 0;
+}
+
+
+
+
+bool is_a_course(const string& userInput) {
+
+    ifstream inFS;
+    string courseName = "";
+    inFS.open("course_names.txt");
+    if (!inFS.is_open()) {
+        cout << "Error opening file << endl";
+        return false;
+    }
+    while (getline(inFS, courseName)) {
+        if (userInput == courseName) {
+            return true;
+        }
+    }
+    return false;
+
+
 }
