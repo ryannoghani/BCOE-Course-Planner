c0d3ca814bec94a323a473249c53fa2942cb9ad6
Peyton Chen <pchen215@ucr.edu>
2022-11-05 17:41:40 -0700

    add main

--- main.cpp
+++ main.cpp
@@ -2,13 +2,11 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include "Major.h"
 using namespace std;
 
-
-
-bool is_a_course(const string&);
-void prereqChecker(const string&, const vector<string>&, vector<string>&);
-
+bool is_a_course(const string& userInput);
+void prereqChecker(const string& fileName, const vector<string> takenCourseList, vector<string>& newCourseList);
 
 int main() {
 
@@ -34,26 +32,22 @@ int main() {
         cin >> course;
     }
 
-
     vector<string> prereq_satisfied_course_list;
     prereqChecker(courseListFile, takenCourses, prereq_satisfied_course_list);
 
-
-
+    for(unsigned i = 0; i < prereq_satisfied_course_list.size(); ++i) {
+        cout << prereq_satisfied_course_list.at(i) << endl;
+    }
     
     return 0;
 }
 
-
-
-
 bool is_a_course(const string& userInput) {
-
     ifstream inFS;
     string courseName = "";
     inFS.open("course_names.txt");
     if (!inFS.is_open()) {
-        cout << "Error opening file << endl";
+        cout << "Error opening file" << endl;
         return false;
     }
     while (getline(inFS, courseName)) {
@@ -66,12 +60,44 @@ bool is_a_course(const string& userInput) {
     return false;
 }
 
-
-
 void prereqChecker(const string& fileName, const vector<string> takenCourseList, vector<string>& newCourseList) {
+    Major majorCourseList(fileName);
+    bool check;
+    vector<string> temp;
 
+    majorCourseList.ReadFile();
 
+    for(unsigned i = 0; i < majorCourseList.listOfClasses.size(); ++i) { // Traverses through each Course item in the Major vector
+        check = true;
+        if(majorCourseList.listOfClasses.at(i).getSize() != 1) {
+            for(unsigned j = 1; j < majorCourseList.listOfClasses.at(i).getSize(); ++j) { // Sets every item from the Course vector into temp
+                temp.push_back(majorCourseList.listOfClasses.at(i).getPrereqs(j));
+            }
+            for(unsigned j = 0; j < temp.size(); ++j) {
+                for(unsigned k = 0; k < takenCourseList.size(); ++k) {
+                    if(temp.at(j) == takenCourseList.at(k)) {
+                        k = takenCourseList.size();
+                        j = temp.size();
+                    }
+                    else if(j == temp.size() - 1 && k == takenCourseList.size() - 1) {
+                        check = false;
+                    }
+                }
+            }
+            temp.clear();
+        }
+        if(check) {
+            newCourseList.push_back(majorCourseList.listOfClasses.at(i).getName());
+        }
+    }
 
-
-
+    cout << endl << "Courses:" << endl;
+    for(unsigned i = 0; i < takenCourseList.size(); ++i) {
+        for(unsigned j = 0; j < newCourseList.size(); ++j) {
+            if(takenCourseList.at(i) == newCourseList.at(j)) {
+                newCourseList.erase(newCourseList.begin() + j);
+                --j;
+            }
+        }
+    }
 }
